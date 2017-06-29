d057b67ee9a12a369cded3aff88252e0afc6f91a
April Mintac Pineda <aprilmintacpineda@gmail.com>
2017-06-30 00:51:13 +0800

    config

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
-require 'classes/Generate.php';
-require 'classes/Make.php';
+require_once 'classes/Generate.php';
+require_once 'classes/Make.php';
 
 $generate = new Generate(new RecursiveDirectoryIterator(__dir__. '\\raw'));
 $collection = $generate->getCollection();
