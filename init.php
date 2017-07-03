8d9302e7ca157e0a29ede00a652cd44d1f748ccd
April Mintac Pineda <aprilmintacpineda@gmail.com>
2017-07-03 17:53:20 +0800

    used Config class instead of constant; classes autoloading.

--- init.php
+++ init.php
@@ -1,6 +1,13 @@
 <?php
-require_once 'classes/Generate.php';
-require_once 'classes/Make.php';
+spl_autoload_register(function($class) {
+  if(file_exists(__dir__. '\\classes/'. $class. '.php')) {
+    include_once __dir__. '\\classes\\'. $class. '.php';
+  } else if(file_exists(__dir__. '\\config\\'. $class. '.php')) {
+    include_once __dir__. '\\config\\'. $class. '.php';
+  } else {
+    return false;
+  }
+});
 
 $generate = new Generate(new RecursiveDirectoryIterator(__dir__. '\\raw'));
 $collection = $generate->getCollection();
